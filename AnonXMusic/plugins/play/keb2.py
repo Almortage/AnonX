6766efb7b07f948da7c75fbea9ae13d6ee4637db
Moalmortagel <105941198+Almortage@users.noreply.github.com>
2024-03-14 01:36:29 +0200

    Update keb2.py

--- AnonXMusic/plugins/play/keb2.py
+++ AnonXMusic/plugins/play/keb2.py
@@ -5,6 +5,7 @@ from pyrogram import *
 from dotenv import load_dotenv
 from os import getenv
 from AnonXMusic import app
+from pyrogram import filters
 from pyrogram import Client, filters
 from pyrogram.types import *
 from pyrogram.errors import PeerIdInvalid
