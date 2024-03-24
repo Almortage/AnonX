e97f654fdf8459b39ea19dbb386f640408ff650b
Moalmortagel <105941198+Almortage@users.noreply.github.com>
2024-03-25 00:53:32 +0200

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -62,8 +62,12 @@ async def play_commnd(
     slider = None
     plist_type = None
     spotify = None
-    user_id = message.from_user.id if message.from_user else "1121532100"
-    user_name = message.from_user.first_name if message.from_user else "None"
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    audio_telegram = (
+        (message.reply_to_message.audio or message.reply_to_message.voice)
+        if message.reply_to_message
+        else None
     )
     video_telegram = (
         (message.reply_to_message.video or message.reply_to_message.document)
